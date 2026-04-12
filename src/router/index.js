f2e1e2e8193de2eca136f96c3f6318864c738390
ktjq-2605 <kristan2605@gmail.com>
2026-04-13 03:45:21 +0800

    Reogranized files and updated index.js accordingly

--- src/router/index.js
+++ src/router/index.js
@@ -9,36 +9,36 @@ const router = createRouter({
     {
       path: '/',
       name: 'Home',
-      component: () => import('@/views/HomeView.vue'), // CHANGE TO '@/views/patients/HomeView.vue' AFTER FOLDER HAS BEEN CREATED
+      component: () => import('@/views/public/HomeView.vue'), // CHANGE TO '@/views/patients/HomeView.vue' AFTER FOLDER HAS BEEN CREATED
     },
     {
       path: '/clinics',
       name: 'ClinicDirectory',
-      component: () => import('@/views/ClinicDirectoryView.vue'),
+      component: () => import('@/views/public/ClinicDirectoryView.vue'),
     },
     {
       path: '/clinics/:clinicId',
       name: 'ClinicDetail',
-      component: () => import('@/views/ClinicDetailView.vue'),
+      component: () => import('@/views/public/ClinicDetailView.vue'),
     },
 
     // Patient Authentication views
     {
       path: '/register',
       name: 'PatientRegister',
-      component: () => import('@/views/RegisterView.vue'),
+      component: () => import('@/views/patient/RegisterView.vue'),
       meta: { guestOnly: true },
     },
     {
       path: '/login',
       name: 'PatientLogin',
-      component: () => import('@/views/LoginView.vue'),
+      component: () => import('@/views/public/LoginView.vue'),
       meta: { guestOnly: true },
      },
      {
       path: '/forgot-password',
       name: 'ForgotPassword',
-      component: () => import('@/views/ForgotPasswordView.vue'),
+      component: () => import('@/views/public/ForgotPasswordView.vue'),
       meta: { guestOnly: true },
     },
 
@@ -46,18 +46,18 @@ const router = createRouter({
     {
       path: '/patient/dashboard',
       name: 'PatientDashboard',
-      component: () => import('@/views/DashboardView.vue'),
+      component: () => import('@/views/patient/DashboardView.vue'),
     },
     {
       path: '/patient/profile',
       name: 'PatientProfile',
-      component: () => import('@/views/ProfileView.vue'),
+      component: () => import('@/views/patient/ProfileView.vue'),
       meta: { requiresAuth: true, role: 'patient' },
     },
     {
       path: '/patient/records',
       name: 'PatientRecords',
-      component: () => import('@/views/RecordsView.vue'),
+      component: () => import('@/views/patient/RecordsView.vue'),
       meta: { requiresAuth: true, role: 'patient' },
     },
 
@@ -65,13 +65,13 @@ const router = createRouter({
     {
       path: '/clinic/register',
       name: 'ClinicRegister',
-      component: () => import('@/views/ClinicRegisterView.vue'),
+      component: () => import('@/views/clinic/ClinicRegisterView.vue'),
       meta: { guestOnly: true },
     },
     {
        path: '/clinic/login',
        name: 'ClinicLogin',
-       component: () => import('@/views/LoginView.vue'),
+       component: () => import('@/views/public/LoginView.vue'),
        meta: { guestOnly: true },
      },
 
@@ -79,37 +79,37 @@ const router = createRouter({
     {
       path: '/clinic/dashboard',
       name: 'ClinicDashboard',
-      component: () => import('@/views/QueueDashboardView.vue'),
+      component: () => import('@/views/clinic/QueueDashboardView.vue'),
       meta: { requiresAuth: true, role: 'clinic' },
     },
     {
       path: '/clinic/analytics',
       name: 'ClinicAnalytics',
-      component: () => import('@/views/ClinicAnalyticsView.vue'),
+      component: () => import('@/views/clinic/ClinicAnalyticsView.vue'),
       meta: { requiresAuth: true, role: 'clinic' },
     },
     {
       path: '/clinic/profile',
       name: 'ClinicProfile',
-      component: () => import('@/views/ClinicProfileView.vue'),
+      component: () => import('@/views/clinic/ClinicProfileView.vue'),
       meta: { requiresAuth: true, role: 'clinic' },
     },
     {
       path: '/clinic/post-consult',
       name: 'PostConsultList',
-      component: () => import('@/views/PostConsultListView.vue'),
+      component: () => import('@/views/clinic/PostConsultListView.vue'),
       meta: { requiresAuth: true, role: 'clinic' },
     },
     {
       path: '/clinic/post-consult/form',
       name: 'PostConsult',
-      component: () => import('@/views/PostConsultView.vue'),
+      component: () => import('@/views/clinic/PostConsultView.vue'),
       meta: { requiresAuth: true, role: 'clinic' },
     },
     {
       path: '/clinic/medical-status',
       name: 'MedicalStatus',
-      component: () => import('@/views/MedicalStatusView.vue'),
+      component: () => import('@/views/clinic/MedicalStatusView.vue'),
       meta: { requiresAuth: true, role: 'clinic' },
     },
   ],
